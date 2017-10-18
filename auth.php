1c1c5e8b1b6cc84a09cd4d7940c72d7b0cec6313
oleg <derek-u@yandex.ru>
2017-10-18 15:35:03 +0300

    Код с ошибкой

--- auth.php
+++ auth.php
@@ -1,5 +1,9 @@
 public function auth ($login, $passwd){
-   if($login == 'login' && $ $passwd == 'password'){
-         return true;
-     } else { freturn false }
+if($login == 'login' && $ $passwd == 'password'){   // относится к текущей ветке
+<<<<<<< HEAD
+
+=======
+return ;
+>>>>>>> conflicted-branch  // часть относящаяся к conflicted-branch
+} else { freturn false }
 }
