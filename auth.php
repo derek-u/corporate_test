17a7762f3733246991912fb95c0ede12bc81205c
oleg <derek-u@yandex.ru>
2017-10-18 15:58:12 +0300

    Код с ошибкой

--- auth.php
+++ auth.php
@@ -1,9 +1,5 @@
 public function auth ($login, $passwd){
-if($login == 'login' && $ $passwd == 'password'){   // относится к текущей ветке
-<<<<<<< HEAD
-
-=======
-return ;
->>>>>>> conflicted-branch  // часть относящаяся к conflicted-branch
-} else { freturn false }
+if($login == 'login' && $ $passwd == 'password'){
+    return true;
+    } else { return false; }
 }
